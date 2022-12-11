c4b302649f0ebb644c284cd4653f4ad4cdeffa15
PratimaGupta10 <guptapratima98710@gmail.com>
2022-12-11 15:15:54 +0530

    user model design

---
+++ models/user.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const thisSchema = new Schema(
+  {
+
+    name: {
+      type: String,
+    },
+    username:{
+        type: String,
+    },
+    email: {
+      type: String,
+    },
+    street: {
+      type: String
+    },
+    suite:{
+        type: String
+    },
+    city:{
+        type: String
+    },
+    zipcode: {
+      type: String,
+    },
+    lat:{
+        type: String,
+    },
+    lng:{
+        type: String,
+    },
+    phone:{
+        type: String,
+    },
+    website:{
+        type: String,
+    },
+    compny_name :{
+        type: String,
+    },
+    catchPhrase:{
+        type: String,
+    },
+    bs:{
+        type: String,
+    }
+   
+
+  },
+
+
+
+  { timestamps: true }
+);
+
+
+module.exports = mongoose.model("user", thisSchema);
